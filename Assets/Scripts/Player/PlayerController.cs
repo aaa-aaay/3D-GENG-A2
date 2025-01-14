f20651fd2ea652b7e509e8864423c2185b245680
aaa-aaay <chujiaming3@gmail.com>
2025-01-14 20:05:07 +0800

    Animations and 3D camera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,61 +8,69 @@ public class PlayerController : MonoBehaviour
     private PlayerInput _playerInput;
     private InputActionAsset _inputActions;
 
-    //private Animator _animator;
+    private Animator _animator;
     private CharacterController _cc;
-    private AnimationManager _animManager;
+    // private AnimationManager _animManager;
 
-    private bool isAttacking, isjumping, isRolling;
+    float velocity;
+    [SerializeField] private float acceleration = 0.1f;
+    [SerializeField] private float deceleration = 0.5f;
+    InputAction attackAction, sprintAction, dodgeAction;
 
     void Start()
     {
-       // _animator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
         _cc = GetComponent<CharacterController>();
         _playerInput = GetComponent<PlayerInput>();
-        _animManager = GetComponent<AnimationManager>();
         _inputActions = _playerInput.actions;
+        attackAction = _inputActions["Attack"];
+        sprintAction = _inputActions["Sprint"];
+        dodgeAction = _inputActions["Roll"];
+
     }
     void Update()
     {
+        Movements();
 
+        if (attackAction.WasPressedThisFrame()) _animator.SetTrigger("Punch");
+        if (dodgeAction.WasPressedThisFrame()) _animator.SetTrigger("Roll");
+    }
+
+    private void Movements()
+    {
         Vector2 input = _inputActions["Movement"].ReadValue<Vector2>();
         Vector3 moveDirection = new Vector3(input.x, 0, input.y);
 
         if (moveDirection.magnitude > 0)
         {
-            _animManager.PlayAnimation(AnimationManager.AniState.RUN);
-
+            _animator.SetBool("IsWalking", true);
             moveDirection = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * moveDirection;
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 3f);
 
         }
-        else if (_inputActions["Attack"].WasPressedThisFrame())
+        else
         {
-            _animManager.PlayAnimation(AnimationManager.AniState.ATTACK);
-            isAttacking = true;
-            Invoke("AttackComplete", _animManager.animator.GetCurrentAnimatorClipInfo(0).Length);
+            _animator.SetBool("IsWalking", false);
         }
-        else if (_inputActions["Roll"].IsPressed())
-        {
-            _animManager.PlayAnimation(AnimationManager.AniState.ROLL);
-            isRolling = true;
 
-        }
-        else if(!isAttacking && !isRolling)
-        {
 
-            _animManager.PlayAnimation(AnimationManager.AniState.IDLE);
+        if (sprintAction.IsPressed() && velocity < 5.0)
+        {
+            velocity += Time.deltaTime * acceleration;
         }
+        else if (!sprintAction.IsPressed() && velocity > 0)
+        {
+            velocity -= Time.deltaTime * deceleration;
+            _animator.SetBool("IsRunning", false);
+        }
+
+        _animator.SetFloat("Velocity", velocity);
     }
+
     private void OnAnimatorMove()
     {
-        Vector3 velocity = _animManager.animator.deltaPosition;
+        Vector3 velocity = _animator.deltaPosition;
         _cc.Move(velocity);
-    }
-
-    void AttackComplete()
-    {
-        isAttacking = false;
     }
 }
